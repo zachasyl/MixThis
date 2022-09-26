ef80cdb561c0b53bd23577bdb8c8b78986337b89
zachasyl <sylvane.z@northeastern.edu>
2022-09-26 17:27:32 -0400

    networkutil GET request progress, still not parsing JSON correctly in SearchDrinks

---
+++ app/src/main/java/cocktail/mixthis/NetworkUti.java
@@ -0,0 +1,40 @@
+package cocktail.mixthis;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class NetworkUti {
+
+    public static String convertStreamToString(InputStream inputStream){
+        StringBuilder stringBuilder=new StringBuilder();
+        try {
+            BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(inputStream));
+            String len;
+            while((len=bufferedReader.readLine())!=null){
+                stringBuilder.append(len);
+            }
+            bufferedReader.close();
+            return stringBuilder.toString().replace(",", ",\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public static String httpResponse(URL url) throws IOException {
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+
+        conn.connect();
+
+        InputStream inputStream = conn.getInputStream();
+        String resp = NetworkUti.convertStreamToString(inputStream);
+
+        return resp;
+    }
+}
--- app/src/main/java/cocktail/mixthis/SearchDrinks.java
+++ app/src/main/java/cocktail/mixthis/SearchDrinks.java
@@ -1,9 +1,25 @@
 package cocktail.mixthis;
 
-import android.os.Bundle;
-import android.widget.EditText;
-
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.net.URL;
+import java.io.IOException;
+
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
 
 
 /**
@@ -13,6 +29,10 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SearchDrinks extends AppCompatActivity {
     //  Declaring the user text entry EditText, which will be used for searches
     private EditText user_Drink;
+    private String actual_url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+    private String string_format;
+    private Handler textHandler = new Handler();
+    Button search;
 
     /**
      *    On Creation of SearchDrinks activity,
@@ -23,5 +43,77 @@ public class SearchDrinks extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_drinks);
         user_Drink = findViewById(R.id.drinksearchbar);
+//        search = (findViewById(R.id.drinkhandler));
+//        search.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                string_format = actual_url + user_Drink.getText().toString();
+//                runOnRunnableThread(view);
+//            }
+//        });
     }
+
+
+    public void runOnRunnableThread(View view){
+        runnableThread runnableThread = new runnableThread();
+        new Thread(runnableThread).start();
+    }
+    class runnableThread implements Runnable {
+        String one_ingredient = "";
+        String all_ingreditents = "";
+
+
+        @Override
+        public void run() {
+            JSONObject jObject;
+
+            try {
+                URL url = new URL(string_format);
+                String resp = NetworkUti.httpResponse(url);
+
+                jObject = new JSONObject(resp);
+
+                Integer i = 1;
+                while (true) {
+                    if ((jObject.getJSONArray("drinks").getJSONObject(0).get("strDrink")).toString().equalsIgnoreCase(user_Drink.getText().toString())) {
+
+                        String ingredient = "strIngredient" + i;
+                        one_ingredient = jObject.getJSONArray("drinks").getJSONObject(0).get(ingredient).toString();
+
+                        all_ingreditents += one_ingredient + "\n";
+
+                        i += 1;
+                    } else {
+                        break;
+                    }
+                }
+            }catch (IOException | JSONException e) {
+            }
+                textHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        TextView result_view = (TextView) findViewById(R.id.result_textview);
+
+                        if (all_ingreditents.length() > 0) {
+                            result_view.setText((CharSequence) all_ingreditents);
+                        } else {
+                            // Either there were multiple drinks but the first was
+                            // not an exact match, or there was no match.
+                            result_view.setText("No Drink Found.");
+                        }
+                    }
+
+                });
+            }
+        }
+
+
+    //used in place of onclick for now, onclick set in search_dirnks.xml
+    public void handler(View view)   {
+        string_format = actual_url + user_Drink.getText().toString();
+        runOnRunnableThread(view);
+
+    }
+
 }
